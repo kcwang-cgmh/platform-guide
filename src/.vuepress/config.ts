696554e9739d69fe23d5aec8153c06465612cb23
kcwang <kcwang.work@gmail.com>
2024-05-08 17:58:09 +0800

    Fix config base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/platform-guide/",
   lang: "zh-TW",
   title: "數位學習平台操作手冊",
   description: "歡迎使用數位學習平台，這裡是平台的操作文件",
